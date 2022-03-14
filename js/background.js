e8096f8408108dfa95b3fef1fb60ae1af1e84962
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2022-03-14 13:50:33 +0700

    Update UI

--- js/background.js
+++ js/background.js
@@ -11,3 +11,13 @@ chrome.runtime.onInstalled.addListener(function () {
   // this runs ONE TIME ONLY (unless the user reinstalls your extension)
   console.log("The Extension is installed!");
 });
+
+const dayOfWeeks = ['CN', 'Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7'];
+const colorOfDay = ['#00ff00', '#ff8000', '#ff0000']; //green, orange, red color
+const i = new Date().getDay();
+
+/**
+ * Add a badge
+ */
+chrome.action.setBadgeText({ text: dayOfWeeks[i] });
+chrome.action.setBadgeBackgroundColor({ color: colorOfDay[i % 3] });
--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,22 @@
 $(function () {
   LoadSchoolYears();
   LoadSemesters();
-  LoadClasses();
   LoadWeeks();
+  LoadClasses();
   LoadLecturers();
+  LoadDepartments();
+  LoadRooms();
 })
 
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
 let month = currentDate.getMonth();
-let selectedClassName = "current";
+let selectedClassName = "active";
+
+btnClass.addEventListener('click', HandleButtonClick);
+btnLecturer.addEventListener('click', HandleButtonClick);
+btnRoom.addEventListener('click', HandleButtonClick);
+btnDepartment.addEventListener('click', HandleButtonClick);
 
 function HandleButtonClick(e) {
   let current = e.target.parentElement.querySelector(
@@ -17,14 +24,40 @@ function HandleButtonClick(e) {
   );
   if (current && current !== e.target) {
     current.classList.remove(selectedClassName);
+    current.disabled = false;
   }
-  e.target.classList.add(selectedClassName);
-}
 
-function AddClickEvent(){
-  $('.groupBtn').find('button').click(function(){
-    
-  });
+  switch (e.target.id) {
+    case 'btnClass':
+      $('.class').show();
+      $('.lecturer').hide();
+      $('.room').hide();
+      $('.department').hide();
+      break;
+    case 'btnLecturer':
+      $('.class').hide();
+      $('.lecturer').show();
+      $('.room').hide();
+      $('.department').hide();
+      break;
+    case 'btnRoom':
+      $('.class').hide();
+      $('.lecturer').hide();
+      $('.room').show();
+      $('.department').hide();
+      break;
+    case 'btnDepartment':
+      $('.class').hide();
+      $('.lecturer').hide();
+      $('.room').hide();
+      $('.department').show();
+      break;
+    default:
+      break;
+  }
+
+  e.target.classList.add(selectedClassName);
+  e.target.disabled = true;
 }
 
 function LoadSchoolYears() {
@@ -48,32 +81,18 @@ function LoadSemesters() {
 }
 
 function LoadClasses() {
-  let schoolyear = $('#cbxSchoolyear').val();
-  let semester = $('#cbxSemester').val();
-  $("#cbxClass").empty();
-  chrome.storage.sync.get('Classes', function (json) {
-    if (json && json.Classes && json.Classes.length > 0) {
-      $.each(json.Classes, function (key, value) {
-        $("#cbxClass").append($('<option>', {
-          value: value.name,
-          text: value.name
-        }));
-      });
-      return;
-    }
-  });
-  $.get("http://qlgd.dlu.edu.vn", function (data) {
-    let temp = $($.parseHTML(data));
-    let json = []
-    temp.find('#ClassStudentID option').each(function () {
-      $('#cbxClass').append($('<option>', {
-        value: $(this).val(),
-        text: $(this).val()
-      }));
-      json.push({ 'name': $(this).val() });
+  let ok = GetAndLoadData('Classes', '#cbxClass');
+  if (!ok)
+    $.get("http://qlgd.dlu.edu.vn", function (html) {
+      let dom = $($.parseHTML(html));
+      let options = dom.find('#ClassStudentID option').get();
+      let data = options.map(item => item.value);
+      SaveAndLoadData(data, 'Classes', '#cbxClass');
     });
-    chrome.storage.sync.set({ 'Classes': json });
-  });
+  // Lỗi cần phải xác thực trước khi gọi
+  //
+  // let schoolyear = $('#cbxSchoolyear').val();
+  // let semester = $('#cbxSemester').val();
   // $.getJSON("http://qlgd.dlu.edu.vn/Home/GetClassStudentByTerm/" + schoolyear + "$" + semester).done(function (data) {
   //   $("#cbxClass").empty();
   //   $.each(data, function (key, value) {
@@ -88,28 +107,105 @@ function LoadClasses() {
 function LoadWeeks() {
   let schoolyear = $('#cbxSchoolyear').val();
   let semester = $('#cbxSemester').val();
-  $.getJSON('http://qlgd.dlu.edu.vn/Public/GetWeek/' + schoolyear + "$" + semester).done(function (data) {
-    $('#cbxWeek').empty();
-    $.each(data, function (key, value) {
-      $('#cbxWeek').append($('<option>', {
-        value: value.Week,
-        text: value.DisPlayWeek
-      }));
-    })
-    $('#cbxWeek').val(data[0].WeekOfYear);
-  });
+  let ok = GetAndLoadData('Weeks', '#cbxWeek', 'Week', 'DisPlayWeek');
+  if (!ok)
+    $.getJSON('http://qlgd.dlu.edu.vn/Public/GetWeek/' + schoolyear + "$" + semester).done(function (data) {
+      SaveAndLoadData(data, 'Weeks', '#cbxWeek', 'Week', 'DisPlayWeek');
+      $('#cbxWeek').val(data[0].WeekOfYear);
+    });
 }
 
 function LoadLecturers() {
   let schoolyear = $('#cbxSchoolyear').val();
   let semester = $('#cbxSemester').val();
-  $.getJSON('http://qlgd.dlu.edu.vn/Public/GetProfessorByTerm/' + schoolyear + "$" + semester).done(function (data) {
-    $('#cbxLecturer').empty();
-    $.each(data, function (key, value) {
-      $('#cbxLecturer').append($('<option>', {
-        value: value.ProfessorID,
-        text: value.ProfessorName
-      }));
-    })
+  let ok = GetAndLoadData('Lecturers', '#cbxLecturer', 'ProfessorID', 'ProfessorName');
+  if (!ok)
+    $.getJSON('http://qlgd.dlu.edu.vn/Public/GetProfessorByTerm/' + schoolyear + "$" + semester).done(function (data) {
+      SaveAndLoadData(data, 'Lecturers', '#cbxLecturer', 'ProfessorID', 'ProfessorName');
+    });
+}
+
+function LoadDepartments() {
+  let schoolyear = $('#cbxSchoolyear').val();
+  let semester = $('#cbxSemester').val();
+  let ok = GetAndLoadData('Departments', '#cbxDepartment', 'FacultyID2', 'FacultyName');
+  if (!ok)
+    $.getJSON('http://qlgd.dlu.edu.vn/Public/GetDepartmentIDByTerm/' + schoolyear + "$" + semester).done(function (data) {
+      data.sort((a, b) => sort(a.FullName, b.FullName));
+      SaveAndLoadData(data, 'Departments', '#cbxDepartment', 'FacultyID2', 'FacultyName');
+    });
+}
+
+function LoadRooms() {
+  let schoolyear = $('#cbxSchoolyear').val();
+  let semester = $('#cbxSemester').val();
+  let ok = GetAndLoadData('Rooms', '#cbxRoom', 'RoomID', 'FullName');
+  if (!ok)
+    $.getJSON('http://qlgd.dlu.edu.vn/Public/GetRoomIDByTerm/' + schoolyear + "$" + semester).done(function (data) {
+      data.sort((a, b) => sort(a.FullName, b.FullName));
+      SaveAndLoadData(data, 'Rooms', '#cbxRoom', 'RoomID', 'FullName');
+    });
+}
+
+/**
+ * Get data in local storage and populate it into select tag HTML
+ * @param {String} keyStorage Distrinct key to identify
+ * @param {String} idSelectTagHtml ID of select tag HTML
+ * @param {String} itemValue Name of a field in data
+ * @param {String} itemText Name of a field in data
+ * @returns Check whether if a 'keyStorage' key existed in local storage
+ */
+function GetAndLoadData(keyStorage, idSelectTagHtml, itemValue, itemText) {
+  let flag = false;
+  chrome.storage.local.get(keyStorage, function (data) {
+    flag = data && data[keyStorage] && data[keyStorage].length > 0;
+    if (flag) {
+      $(idSelectTagHtml).empty();
+      $.each(data[keyStorage], function (index, value) {
+        _val = itemValue ? value[itemValue] : value;
+        _text = itemText ? value[itemText] : value;
+        $(idSelectTagHtml).append($('<option>', {
+          value: _val,
+          text: _text
+        }));
+      });
+    }
   });
+  return flag;
+}
+
+/**
+ * Populate data to select tag HTML and save them into local storage
+ * @param {Array} data Array of object
+ * @param {String} keyStorage Distrinct key to identify
+ * @param {String} idSelectTagHtml ID of select tag HTML
+ * @param {String} itemValue Name of a field in data
+ * @param {String} itemText Name of a field in data
+ */
+function SaveAndLoadData(data, keyStorage, idSelectTagHtml, itemValue, itemText) {
+  let json = [];
+  $(idSelectTagHtml).empty();
+  $.each(data, function (index, value) {
+    _val = itemValue ? value[itemValue] : value;
+    _text = itemText ? value[itemText] : value;
+    $(idSelectTagHtml).append($('<option>', {
+      value: _val,
+      text: _text
+    }));
+    if (itemValue && itemText)
+      json.push({ itemValue: _val, itemText: _text });
+    else
+      json.push(_val);
+  });
+  let content = {};
+  content[keyStorage] = json;
+  chrome.storage.local.set(content);
+}
+
+function sort(a, b) {
+  if (a > b)
+    return 1;
+  else if (a < b)
+    return -1;
+  return 0;
 }
